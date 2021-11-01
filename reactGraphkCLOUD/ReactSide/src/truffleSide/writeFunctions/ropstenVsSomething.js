ca258c4b2c0ed36e2aeac521970d8a962f330f44
Yessenbayev Olzhas <ollie041114@gmail.com>
2021-11-01 20:40:40 +0900

    Hopefully

--- reactGraphkCLOUD/ReactSide/src/truffleSide/writeFunctions/ropstenVsSomething.js
+++ reactGraphkCLOUD/ReactSide/src/truffleSide/writeFunctions/ropstenVsSomething.js
@@ -8,14 +8,15 @@ var ropsten = {
   contractAddress: require('..\\..\\abi\\KCLOUD.json')['networks']['3']['address'],
   infura: 'https://ropsten.infura.io/v3/f54c17f8fd334d78bcb2117202fe7ce0'
 };
+
 // Create a JavaScript representation of the smart contract
-var local = {
-  account1: "0x070aE2b66a63De8b4Cd352e725CA81Ed663611F0",
-  privateKey1: Buffer.from('7ae4495b934af72e8ce1d5792f98c119f1d831690ee27dcfeee4c077d7f4f7b3', 'hex'),
-  contractJSON: require('..\\..\\abi\\KCLOUD.json'),
-  contractABI: require('..\\..\\abi\\KCLOUD.json'),
-  contractAddress: require('..\\..\\abi\\KCLOUD.json')['networks']['3']['address'],
-  infura: 'https://ropsten.infura.io/v3/f54c17f8fd334d78bcb2117202fe7ce0'
+var localDung = {
+  account1: "0x070aE2b66a63De8b4Cd352e725CA81Ed663611F0", // just google how to find from Truffle
+  privateKey1: Buffer.from('7ae4495b934af72e8ce1d5792f98c119f1d831690ee27dcfeee4c077d7f4f7b3', 'hex'), // just google how to find from Truffle
+  contractJSON: require('..\\..\\abi\\KCLOUD.json'), // just google how to find from Truffle
+  contractABI: require('..\\..\\abi\\KCLOUD.json'), // just google how to find from Truffle 
+  contractAddress: require('..\\..\\abi\\KCLOUD.json')['networks']['3']['address'], // just google how to find from Truffle
+  infura: 'https://ropsten.infura.io/v3/f54c17f8fd334d78bcb2117202fe7ce0' // I have no idea? Google "Connect web3 with local truffle Ethereum"
 };
 
 var network = ropsten;
